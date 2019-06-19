b37bd058778a3dddcb876bdd177569d357aec8c7
nikita-lysenko <nikita.lysenko@spryker.com>
2019-06-19 16:55:52 +0300

    characters fetch and render

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,44 @@
 import * as React from "react";
 
+const apiUrl = "https://www.anapioficeandfire.com/api";
+
 export class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: [],
+      isFetching: true,
+      error: null
+    };
+  }
+
+  componentDidMount() {
+    fetch(`${apiUrl}/characters?page=1&pageSize=10`)
+      .then(res => res.json())
+      .then(result => this.setState({ data: result, isFetching: false }));
   }
 
   render() {
-    return <div>test</div>;
+    if (this.state.isFetching) {
+      return <h2>Loading...</h2>;
+    } else {
+      return (
+        <div>
+          {this.state.data.map(character => {
+            for (let prop in character) {
+              console.log(prop);
+              if (character[prop] != false) {
+                return (
+                  <div>
+                    <span>{prop}:</span>
+                    <span>{character[prop]}</span>
+                  </div>
+                );
+              }
+            }
+          })}
+        </div>
+      );
+    }
   }
 }
